e4b54e54672c9bf9bc199a7a128d34bde22ef711
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-21 16:41:50 +0000

    test table truncates before everytest

--- models/bookmarks.rb
+++ models/bookmarks.rb
@@ -2,7 +2,11 @@ require 'pg'
 class Bookmarks 
 
   def self.all
+    if ENV['RACK_ENV'] == 'test'
+      conn = PG.connect(dbname: 'bookmark_manager_test')
+    else
     conn = PG.connect( dbname: 'bookmark_manager' )
+    end
     rs = conn.exec( "SELECT * FROM bookmarks" )
     rs.map { |row| row['url'] }
   end
---
+++ spec/setup_test_database.rb
@@ -0,0 +1,8 @@
+require 'pg'
+
+
+
+def clear_table
+  conn = PG.connect( dbname: 'bookmark_manager_test' )
+  conn.exec( "TRUNCATE TABLE bookmarks;" )
+end
