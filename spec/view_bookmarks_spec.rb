03cdf21333cbd67a027b702c0164d598415557e2
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-22 13:44:47 +0000

    refactor unit tests

--- spec/view_bookmarks_spec.rb
+++ spec/view_bookmarks_spec.rb
@@ -4,10 +4,9 @@ require 'pg'
 describe Bookmarks do
 
   it 'returns a list of saved bookmarks' do
-    conn = PG.connect(dbname: 'bookmark_manager_test')
 
-    conn.exec( "INSERT INTO bookmarks (url) VALUES ('www.newyorker.com'), ('www.twitter.com')" )
-
+    Bookmarks.create("www.newyorker.com")
+    Bookmarks.create("www.twitter.com")
     expect(Bookmarks.all).to include("www.newyorker.com")
     expect(Bookmarks.all).to include("www.twitter.com")
   end
