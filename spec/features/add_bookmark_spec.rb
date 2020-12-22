eabfc1e120b76c4778dd7889c5f62a1149b752b4
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-22 12:23:16 +0000

    saves adding bookmarks into params from the field

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -3,4 +3,12 @@ feature 'users can add a bookmark' do
     visit('/add')
     expect(page).to have_content 'save new bookmarks here'
   end
+
+  scenario 'allows a user to fill in a form to add a bookmark' do
+    visit('/add')
+    fill_in('url', :with => 'www.asda.com')
+    click_on('add bookmark')
+    visit('/bookmarks')
+    expect(page).to have_content 'www.asda.com'
+  end
 end
